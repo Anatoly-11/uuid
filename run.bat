ce44b05ada3406187ca0a0f952de4e02e06ba6b0
Анатолий <ka_anat@mail.ru>
2023-11-02 05:22:27 +0300

    подправлен батник для дистрибуции в роект rnd

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+@echo off
 del /q distr\*.*
 copy headers\uuid.h  distr
 copy x64\Debug\uuid.dll  distr
@@ -5,8 +6,11 @@ copy x64\Debug\uuid.lib  distr
 
 copy distr\uuid.h ..\testUUID\headers
 copy distr\uuid.lib ..\testUUID\lib
+copy distr\uuid.dll ..\testUUID\x64\Debug
+
+copy distr\uuid.h ..\rnd\headers
+copy distr\uuid.lib ..\rnd\lib
 
 copy distr\uuid.h ..\matrix\headers
 copy distr\uuid.lib ..\matrix\lib
 copy distr\uuid.dll ..\matrix\x64\Debug
-
